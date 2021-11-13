a491d39f34ed50abf847ab3815fea028d74934ce
shivangisingh <Shivangiarusingh@gmail.com>
2021-11-13 21:28:21 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { TododetailsComponent } from './tododetails/tododetails.component';
+import { SpaceXComponent } from './space-x/space-x.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TododetailsComponent,
+    SpaceXComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/space-x/space-x.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-x',
+  templateUrl: './space-x.component.html',
+  styleUrls: ['./space-x.component.css']
+})
+export class SapceXComponent implements OnInit {
+  data:any;
+  link = 'https://api.spacexdata.com/v3/missions';
+  constructor(private http:HttpClient) { }
+  ngOnInit(): void {
+    this.http.get(this.link).subscribe((responseData:any)=> {
+      this.data = responseData;
+    })
+  }
+}
